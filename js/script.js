e33071d8ac4ca65b9f958f2c46514d10eb209f64
Ghaitaoui Moulay El hadj <64228972+moulayghaitaoui@users.noreply.github.com>
2022-09-30 20:48:45 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*============typing an  animation==========*/
+var typing = new Typed(".typing", {
+    strings: ["", " Mobile Developer", " Designer UI | UX", " Graphic Designer", " Desktop Developer"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+})
